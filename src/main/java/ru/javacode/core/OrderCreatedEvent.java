9172254c8c74224d5c83e1f0ca893614cfe9074c
TujhZreitd <egor.yakushev.96@bk.ru>
2024-11-19 12:24:28 +0300

    commit init

---
+++ src/main/java/ru/javacode/core/OrderCreatedEvent.java
@@ -0,0 +1,17 @@
+package ru.javacode.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderCreatedEvent {
+    private String productId;
+    private String nameProduct;
+    private BigDecimal price;
+    private Integer quantity;
+}
